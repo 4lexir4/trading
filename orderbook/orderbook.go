f228f0c4fafa788c69aa446d0e5587782a74d6de
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2024-01-12 14:12:36 +1100

    orderbook

--- orderbook/orderbook.go
+++ orderbook/orderbook.go
@@ -1,1 +1,44 @@
 package orderbook
+
+import (
+	"crypto/rand"
+	"time"
+)
+
+type Limit struct {
+	price       float64
+	orders      []*Order
+	totalVolume float64
+}
+
+func NewLimit(price float64) *Limit {
+	return &Limit{
+		price:  price,
+		orders: []*Order{},
+	}
+
+}
+
+type Order struct {
+	id        int64
+	size      float64
+	timestamp int64
+	isBid     bool
+}
+
+func NewOrder(isBid bool, size float64) *Order {
+	return &Order{
+		id:        rand.Intn(100_000), // TODO fix later...
+		size:      size,
+		timestamp: time.Now().UnixNano(),
+		isBid:     isBid,
+	}
+}
+
+func NewBidOrder(size float64) *Order {
+	return NewOrder(true, size)
+}
+
+func NewAskOrder(size float64) *Order {
+	return NewOrder(false, size)
+}
