0c9ac2198302d2b5c02b6207821e2b4e9097ccec
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2024-01-21 15:08:45 +1100

    kraken provider

--- providers/kraken.go
+++ providers/kraken.go
@@ -1,3 +1,9 @@
 package providers
 
-type   r
+import "github.com/4lexir4/trading/orderbook"
+
+type KrakenProvider struct {
+	Orderbooks orderbook.Orderbooks
+	symbols    []string
+	feedch     chan orderbook.DataFeed
+}
