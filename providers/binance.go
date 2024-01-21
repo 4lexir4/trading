723fed6d1bc978054e2dd2cf2232932a99244b6a
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2024-01-21 14:56:37 +1100

    new binance orderbook

--- providers/binance.go
+++ providers/binance.go
@@ -7,3 +7,15 @@ type BinanceProvider struct {
 	symbols    []string
 	feedch     chan orderbook.DataFeed
 }
+
+func NewBinanceOrderbooks(feedch chan orderbook.DataFeed, symbols []string) *BinanceProvider {
+	books := orderbook.Orderbooks{}
+	for _, symbol := range symbols {
+		books[symbol] = orderbook.NewBook(symbol)
+	}
+	return &BinanceProvider{
+		Orderbooks: books,
+		symbols:    symbols,
+		feedch:     feedch,
+	}
+}
