08ceb4ed45d53c1a56d099bdc244b4bc65f67ab2
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2024-01-21 15:10:23 +1100

    new kraken provider

--- providers/kraken.go
+++ providers/kraken.go
@@ -7,3 +7,15 @@ type KrakenProvider struct {
 	symbols    []string
 	feedch     chan orderbook.DataFeed
 }
+
+func NewKrakenProvider(feedch chan orderbook.DataFeed, symbols []string) *KrakenProvider {
+	books := orderbook.Orderbooks{}
+	for _, symbol := range symbols {
+		books[symbol] = orderbook.NewBook(symbol)
+	}
+	return &KrakenProvider{
+		Orderbooks: books,
+		symbols:    symbols,
+		feedch:     feedch,
+	}
+}
