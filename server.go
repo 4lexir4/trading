51d71dca35c94278a54b2822c3e5a5308e592f12
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2024-01-23 08:53:16 +1100

    type, start and handle

--- server.go
+++ server.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+type Server struct {
+	conn map[*websocket.Conn]bool
+}
+
+func (s *Server) Start() error {
+	http.HandleFunc("/bestspreads", s.handleBestSpreads)
+	return http.ListenAndServe(":3000", nil)
+}
+
+func (s *Server) handleBestSpreads(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Panicln("websocket upgrade error:", err)
+		return
+	}
+	defer ws.Close()
+}
